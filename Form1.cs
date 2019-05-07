642cc45b625b231877753ef147af5e5f110c8ed4
ishibashi.futos <ishibashi.futos@jp.fujitsu.com>
2019-05-07 14:55:26 +0900

    DataGridViewの実装を追加

--- Form1.cs
+++ Form1.cs
@@ -17,6 +17,7 @@ namespace formapp
         private NumericUpDown spin;
         private MenuStrip ms;
         private TextBox text;
+        private DataGridView dataGrid;
 
         public Form1()
         {
@@ -34,6 +35,34 @@ namespace formapp
                 Multiline = true,
                 ScrollBars = ScrollBars.Vertical,
             });
+
+            // TODO: Columnのサイズを最大化する
+            this.dataGrid = new DataGridView{
+                Name = "DataGridView",
+                Location = new Point(10, 160),
+                Size = new Size(780, 100),
+                RowHeadersVisible = false,
+                SelectionMode = DataGridViewSelectionMode.FullRowSelect,
+                MultiSelect = false,
+                ColumnCount = 2
+            };
+
+            this.dataGrid.Columns[0].Name = "Time";
+            this.dataGrid.Columns[1].Name = "Result";
+
+            for (var i = 1; i <= 10; i++)
+            {
+                string[] s = {i.ToString(), string.Format("result{0}",i)};
+                this.dataGrid.Rows.Add(s);
+            }
+
+            this.Controls.Add(this.dataGrid);
+
+            for (var i = 0; i < this.dataGrid.Columns.Count; i++)
+            {
+                this.dataGrid.Columns[i].AutoSizeMode = DataGridViewAutoSizeColumnMode.AllCells;
+            }
+
 
             this.Controls.Add(spin = new NumericUpDown{
                 Location = new Point(10, 300),
